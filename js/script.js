e796975d85ef96e6d0bd4af1e8135c208307e674
marco bottoni <bottoni.mrc@gmail.com>
2020-12-18 15:13:21 +0100

    boolflix 1.1.1

---
+++ js/script.js
@@ -0,0 +1,30 @@
+var app = new Vue({
+  el: "#app",
+  data: {
+    movies:[],
+    keywordMovieTitle: ""
+  },
+  methods: {
+    apiSearch:function () {
+      console.log("test");
+      const self = this;
+
+      axios.get('https://api.themoviedb.org/3/search/movie',{
+        params:{
+          api_key:'1a9e369e17c6561e8db673b4313eb4c1',
+          query:self.keywordMovieTitle,
+          language:'it-IT',
+        }
+      })
+        .then(function (result) {
+          let movieSpecs = result.data.results;
+          self.movies = movieSpecs;
+          console.log(self.movies);
+        });
+
+    }
+  },
+  created: function (){
+
+  }
+})
