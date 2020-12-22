720be2c597c39241e5f82e1bc2ad6b1db398d027
marco bottoni <bottoni.mrc@gmail.com>
2020-12-22 14:44:32 +0100

    boolflix 2.2.6

--- js/script.js
+++ js/script.js
@@ -2,15 +2,13 @@ var app = new Vue({
   el: "#app",
   data: {
     movies:[],
-    series:[],
     keywordMovieTitle: "",
     flags:
       {
         es:"https://external-preview.redd.it/sgupg2QyvwFm7eLaH0isYTSx1IAYT2cnG9EG2qaK7dc.png?auto=webp&s=c2fe73665a3b109d9a040fb4f70fcba4e2875149",
         it:"https://upload.wikimedia.org/wikipedia/en/thumb/0/03/Flag_of_Italy.svg/1200px-Flag_of_Italy.svg.png",
         en:"https://upload.wikimedia.org/wikipedia/en/thumb/a/a4/Flag_of_the_United_States.svg/1200px-Flag_of_the_United_States.svg.png"
-    }
-
+    },
   },
   methods: {
     apiSearch:function () {
@@ -37,27 +35,20 @@ var app = new Vue({
       }
       Promise.all([getMovies(), getTv()])
         .then(function (result) {
-          let movieSpecs = result[0].data.results;
-          self.movies = movieSpecs;
-          console.log(self.movies);
+          this.movies = [...result[0].data.results,...result[1].data.results];
+          console.log(this.movies);
           self.starsConversion(self.movies);
-
-          let tvSpecs = result[1].data.results;
-          self.series = tvSpecs;
-          console.log(self.series);
-          self.starsConversion(self.series)
-
         });
         self.keywordMovieTitle = "";
-
     },
     starsConversion: function (movies){
-      for(let i =0; i<movies.length; i++) {
-        const fullStars = Math.ceil(movies[i].vote_average /2);
-        movies[i].fullStars = fullStars;
+      console.log("test", self.movies.length);
+      for(let i =0; i < self.movies.length; i++) {
+        const fullStars = Math.ceil(self.movies[i].vote_average /2);
+        self.movies[i].fullStars = fullStars;
       }
     },
   },
-  created: function (index){
+  created: function (){
   }
 })
