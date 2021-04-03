28383893e8c12c9baaccc23ae1a7b5f373a0aba9
marco bottoni <bottoni.mrc@gmail.com>
2021-04-03 18:39:20 +0200

    boolflix 4.0.1

--- js/script.js
+++ js/script.js
@@ -4,7 +4,8 @@ var app = new Vue({
     movies:[],
     keywordMovieTitle: "",
     flags:['en','it','es'],
-    fiveActors:[],
+    onTheAir:[],
+    airingToday:[],
   },
   methods: {
     apiSearch:function () {
@@ -42,42 +43,101 @@ var app = new Vue({
           console.log(self.movies);
           self.starsConversion(self.movies);
           console.log(self.keywordMovieTitle);
-          self.keywordMovieTitle = "";
-          self.displayCast();
+          // self.keywordMovieTitle = "";
+          // self.displayCast();
         });
         // /merge (movies + series)
 
     },
+    tvOnTheAir: function(){
+      const self = this;
+      self.movies = [];
+      self.keywordMovieTitle = '';
+      self.airingToday = [];
+      axios
+      .get('https://api.themoviedb.org/3/tv/on_the_air',  {
+        params: {
+          api_key: 'e99307154c6dfb0b4750f6603256716d',
+          language: 'it-IT'
+          }
+        }
+      )
+      .then(function(response) {
+          self.onTheAir = response.data.results;
+          self.starsConversion(self.onTheAir);
+  
+          console.log(self.onTheAir)
+  
+        }
+      ); // On the air
+      
+    },
+    airing: function() {
+      const self = this;
+      self.movies = [];
+      self.keywordMovieTitle = '';
+      self.onTheAir = [];
+      
+
+      axios
+        .get('https://api.themoviedb.org/3/tv/airing_today',  {
+          params: {
+            api_key: 'd631125b3120a33333b536f9aa3c36e9',
+            language: 'it-IT'
+            }
+          }
+        )
+        .then(function(response) {
+            self.airingToday = response.data.results;
+            self.starsConversion(self.airingToday);
+
+            // console.log(self.onTheAir)
+            console.log(self.airingToday);
+            console.log(self.airingToday.length);
+          }
+        )
+    }, 
     // vote (stars)
-    starsConversion: function (movies){
-      for(let i =0; i < this.movies.length; i++) {
-        const fullStars = Math.ceil(this.movies[i].vote_average /2);
-        this.movies[i].fullStars = fullStars;
+    starsConversion: function (movies, onTheAir, airingToday ){
+      if(this.movies.length > 0){
+        for(let i =0; i < this.movies.length; i++) {
+          const fullStars = Math.ceil(this.movies[i].vote_average /2);
+          this.movies[i].fullStars = fullStars;
+        }
+      }else if (this.onTheAir.length > 0){
+        for(let i =0; i < this.onTheAir.length; i++) {
+          const fullStars = Math.ceil(this.onTheAir[i].vote_average /2);
+          this.onTheAir[i].fullStars = fullStars;
+        }
+      }else if(this.airingToday.length > 0){
+        for(let i =0; i < this.airingToday.length; i++) {
+          const fullStars = Math.ceil(this.airingToday[i].vote_average /2);
+          this.airingToday[i].fullStars = fullStars;
+        }
       }
       // /vote (stars)
     },
-    // first five actors/actress
-    displayCast: function(){
-      for (var i = 0; i < this.movies.length; i++) {
-        const movieIds = this.movies[i].id;
-        console.log(this.movies[i].id);
-
-        axios.get("https://api.themoviedb.org/3/movie/" + movieIds + "/credits?api_key=1a9e369e17c6561e8db673b4313eb4c1&language=en-US")
-        .then(function(response){
-          const cast = response.data.cast;
-          var actors = []
-          for (var j = 0; j < 5; j++) {
-            var actor = cast[j].name;
-            actors.push(actor)
-          }
-          this.fiveActors = actors;
-          console.log("test", this.fiveActors);
-        })
-      }
-    }
-    // /first five actors/actress
 
   },
-  created: function (){
+  mounted: function (){
+    var self = this;
+    axios
+    .get('https://api.themoviedb.org/3/tv/on_the_air',  {
+      params: {
+        api_key: 'e99307154c6dfb0b4750f6603256716d',
+        language: 'it-IT'
+        }
+      }
+    )
+    .then(function(response) {
+        self.onTheAir = response.data.results;
+        self.starsConversion(self.onTheAir);
+
+        console.log(self.onTheAir)
+
+      }
+    ) // On the air
+
+    
   }
 })
